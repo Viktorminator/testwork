c8688a8dc37736c6b02787ed1bce01ca205170e6
Viktorminator <viktorminator@gmail.com>
2016-02-27 21:18:08 +0100

    Created theme with Stylus, Underscore, Gulp used Styl_s @luclemo

---
+++ custom_functions.php
@@ -0,0 +1,7 @@
+<?php 
+function enqueue_styles_scripts() {
+	wp_enqueue_style('gfonts', '//fonts.googleapis.com/css?family=Open+Sans:400,600');
+}
+
+?>
+ 
---
+++ footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package styl_s
+ */
+
+?>
+
+
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
