dac9e01b7969009497f5f4e5cc2c967d72f1af53
Viktorminator <viktorminator@gmail.com>
2016-03-02 23:30:24 +0100

    Added media-queries + breakpoints

--- custom_functions.php
+++ custom_functions.php
@@ -1,7 +1,5 @@
 <?php 
 function enqueue_styles_scripts() {
-	wp_enqueue_style('gfonts', '//fonts.googleapis.com/css?family=Open+Sans:400,600');
-	wp_enqueue_style('css', '/css/style.css')
 }
 
 ?>
--- index.php
+++ index.php
@@ -17,19 +17,21 @@ get_header(); ?>
 <div class="section hero">
   <div class="container">
     <div class="row">
-      <div class="one-half column">
-        <h1 class="heading">
-          Notify.
+      <div class="one-half column hero__left">
+        <h1 class="hero__heading">
+          Notify<span class="green">.</span>
         </h1>
-        <h4 class="hero-heading">
+        <p>
           A great new free psd theme to showcase your new application.
-        </h4>
-        
+        </p>
+        <div class="hero__icons">
+          <a href="#" class='sprite apple_g'></a>
+          <a href="#" class='sprite android_g'></a>
+          <a href="#" class='sprite wind_g'></a>  
+        </div>
       </div>
-      <div class="one-half column phones">
-        <a href="#" class='sprite android_g'></a>
-        <a href="#" class='sprite apple_g'></a>
-        <a href="#" class='sprite wind_g'></a>      
+      <div class="one-half column hero__app">
+        <img class="hero__app__phone" src="wp-content/themes/Styl_s-master/img/hand.png" alt="">
       </div>
     </div>
   </div>
@@ -39,29 +41,29 @@ get_header(); ?>
   <div class="container">
     <div class="row">
       <div class="one-third column value">
-        <img src="../img/wheel.png" alt="" class="values__icon">
-        <h5 class="value-heading">
+        <img src="wp-content/themes/Styl_s-master/img/wheel.png" alt="" class="values__icon">
+        <h5 class="value__heading">
           Editable Theme
         </h5>
-        <p class="value-description">
+        <p class="value__description">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.
         </p>
       </div>
       <div class="one-third column value">
-        <img src="../img/wheel.png" alt="" class="values__icon">
-        <h5 class="value-heading">
+        <img src="wp-content/themes/Styl_s-master/img/star.png" alt="" class="value__icon">
+        <h5 class="value__heading">
           Flat Design
         </h5>
-        <p class="value-description">
+        <p class="value__description">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.
         </p>
       </div>
       <div class="one-third column value">
-        <img src="../img/wheel.png" alt="" class="values__icon">
-        <h5 class="value-heading">
+        <img src="wp-content/themes/Styl_s-master/img/globe.png" alt="" class="values__icon">
+        <h5 class="value__heading">
           Reach Your Audience
         </h5>
-        <p class="value-description">
+        <p class="value__description">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.
         </p>
       </div>
@@ -72,10 +74,10 @@ get_header(); ?>
 <div class="section notify">
   <div class="container">
     <div class="row">
-      <h2 class="notify__header">
+      <h3 class="notify__header">
         Get Notified Of Any Updates!
-      </h2>
-      <p>
+      </h3>
+      <p class="notify__description">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla nisl congue congue. Maecenas nec condimentum libero, at elementum mauris. Phasellus eget nisi dapibus, ultricies nisl at, hendrerit risusuis ornare luctus id sollicitudin ante lobortis at.
       </p>
     </div>
