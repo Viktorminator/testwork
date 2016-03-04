ddc003a68af41ec0f124d6f7297259c889df98aa
Viktorminator <viktorminator@gmail.com>
2016-03-04 19:13:07 +0100

    Added widget to dashboard with output of subscribed users

---
+++ users-grid.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 3/4/16
+ * Time: 5:06 PM
+ */
+
+add_action('wp_dashboard_setup', 'my_custom_dashboard_widgets');
+
+function my_custom_dashboard_widgets() {
+    global $wp_meta_boxes;
+
+    wp_add_dashboard_widget('custom_help_widget', 'Subscribed users', 'subscribers_grid');
+}
+
+function subscribers_grid() {
+    echo '<b>Hi all! This will output the users grid:</b> <br/>';
+    showSubscribers();
+}
+
+function showSubscribers() {
+    global $wpdb;
+
+    $appTable = $wpdb->prefix . "subscribers";
+    // echo $appTable;
+    $query = $wpdb->prepare("SELECT * FROM $appTable", RID);
+    $subscribers = $wpdb->get_results($query);
+
+    foreach ( $subscribers as $subscriber ) {
+        echo $subscriber->email . "<br/>";
+    }
+}
